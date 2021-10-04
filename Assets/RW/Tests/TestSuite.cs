319e5b3c483561da926988b03e80c93cc3e88dc3
SzymonPiskorz <c00242808@itcarlow.ie>
2021-10-04 11:54:29 +0100

    Added score reset test

--- Assets/RW/Tests/TestSuite.cs
+++ Assets/RW/Tests/TestSuite.cs
@@ -90,4 +90,13 @@ public class TestSuite
         Assert.AreEqual(game.score, 1);
     }
 
+    [UnityTest]
+    public IEnumerator Score0()
+    {
+        game.NewGame();
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(game.score, 0);
+    }
 }
